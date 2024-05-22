2022ad024c3442fc22ecac17e0d31b1aa38b7bd0
Helena Richie <helenarichie@gmail.com>
2024-05-22 16:12:31 -0400

    upload latest version of branch

--- builds/setup.summit.gcc.sh
+++ builds/setup.summit.gcc.sh
@@ -5,6 +5,7 @@
 
 #module load gcc/10.2.0 cuda/11.4.0 fftw hdf5 python
 #module load gcc cuda fftw hdf5 python googletest/1.11.0
-module load gcc cuda/12.2.0 fftw hdf5 python
+# module load gcc cuda/12.2.0 fftw hdf5 python
+module load gcc cuda/12.2.0 hdf5 python ums/default ums-ompix/default openmpi/5.0.3
 
 export CHOLLA_ENVSET=1
