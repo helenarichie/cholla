6c26042209c83ec670fdf740312256b6906275e8
helenarichie <helenarichie@gmail.com>
2024-08-04 11:07:13 -0400

    clang format

--- src/analysis/global_reduce_dust.h
+++ src/analysis/global_reduce_dust.h
@@ -5,11 +5,11 @@
     #include "../global/global.h"
     #include "../utils/gpu.hpp"
 
-void Global_Reduce_Dust(Real *dev_conserved, int nx, int ny, int nz, Real dx, Real dy, Real dz, int n_ghost, int n_fields,
-                      Real *mass_cloud, Real *mass_dust, Real density_cloud_init);
+void Global_Reduce_Dust(Real *dev_conserved, int nx, int ny, int nz, Real dx, Real dy, Real dz, int n_ghost,
+                        int n_fields, Real *mass_cloud, Real *mass_dust, Real density_cloud_init);
 
 __global__ void Global_Reduce_Dust_Kernel(Real *dev_conserved, int nx, int ny, int nz, Real dx, Real dy, Real dz,
-                                        int n_ghost, int n_fields, Real *mass_cloud, Real *mass_dust,
-                                        Real density_cloud_init);
+                                          int n_ghost, int n_fields, Real *mass_cloud, Real *mass_dust,
+                                          Real density_cloud_init);
   #endif  //  GLOBAL_REDUCE_DUST_CUDA_H
 #endif    //  GLOBAL_REDUCE_DUST
