0990f6f0f144bb59d2de66e2b475fd8314445d91
helenarichie <helenarichie@gmail.com>
2024-08-15 18:06:49 -0400

    set disk radius with galaxy model macro

--- src/model/disk_ICs.cpp
+++ src/model/disk_ICs.cpp
@@ -65,7 +65,11 @@ Real Sigma_Disk_D3D(Real r, Real *hdp)
   // return the exponential surface density
   Real Sigma_0 = hdp[9];
   Real R_g     = hdp[10];
+  #ifdef MW_MODEL
+  Real R_c     = 9.9;
+  #else
   Real R_c     = 4.9;
+  #endif
   Real Sigma;
   Real delta = 0.1;
   Real norm  = log(1.0 / 3.0);
