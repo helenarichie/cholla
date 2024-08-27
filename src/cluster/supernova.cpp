6d76b46a9d5b0e4e1d258cbf2a61a89ceb37f235
helenarichie <helenarichie@gmail.com>
2024-08-27 15:18:45 -0400

    print SFR at start-up

--- src/cluster/supernova.cpp
+++ src/cluster/supernova.cpp
@@ -87,6 +87,7 @@ void Supernova::Initialize(Grid3D G, struct Parameters *P)
 
   R_cl        = P->supernova_rcl;
   SFR         = 5000.0;
+  chprintf("SFR: %f (M_sun/yr)\n", SFR/1000);
   supernova_e = P->supernova_e;
 
   dx = H.dx;
