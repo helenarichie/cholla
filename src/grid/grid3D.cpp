a089a7fd6701517ae292560b6cad07570c38e3fe
Helena Richie <helenarichie@gmail.com>
2024-11-05 15:40:53 -0500

    update build

--- src/grid/grid3D.cpp
+++ src/grid/grid3D.cpp
@@ -165,7 +165,7 @@ void Grid3D::Initialize(struct Parameters *P)
   C_cfl = 0.3;
 
 #ifdef AVERAGE_SLOW_CELLS
-  H.min_dt_slow = 0.0326;  // Initialize the minumum dt to a tiny number
+  H.min_dt_slow = 0.016276;  // Initialize the minumum dt to a tiny number
 #endif                   // AVERAGE_SLOW_CELLS
 
 #ifdef CLOUD_TRACKING
