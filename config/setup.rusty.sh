1003bce846807b72509d06635434e75faefcbf10
Helena Richie <hrichie@rusty4.cm.cluster>
2026-09-21 18:56:27 -0400

    add working version of rusty setup files

--- config/setup.rusty.sh
+++ config/setup.rusty.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-module purge
+module --force purge
 
-module load gcc 
-module load cuda/12.8
+module load modules/2.4-20250724
+
+module load gcc
+module load cuda/12.8.0
 module load openmpi
 module load hdf5
